2a5e5e901af101a35b89ee9eea2b16565192f39c
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-10 22:16:19 +0530

    cpp

--- Basic/hello_world.cpp
+++ Basic/hello_world.cpp
@@ -1,34 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 bool next();
 
 int main()
 {
-    // printf("Hi\n");
-    // cout << "Hiiiii MorningStar" << endl;
-    // cout << "Hiiiii MorningStar\n";
+    printf("Hi\n");
+    cout << "Hiiiii MorningStar" << endl;
+    cout << "Hiiiii MorningStar\n";
 
-    // int n ;
-    // scanf("%d",&n);
-    // cout << "scanf == " << n << endl;
+    int n;
+    scanf("%d", &n);
+    cout << "scanf == " << n << endl;
 
-    // cin >> n ;
-    // cout << "cin == " << n << endl;
+    cin >> n;
+    cout << "cin == " << n << endl;
 
-    // int a,b,c;
-    // cin >> a >> b >> c;
-    // cout << a << " " << b << " " << c <<endl;
-    // cout << a+b+c <<endl;
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a << " " << b << " " << c << endl;
+    cout << a + b + c << endl;
 
-    if(next() == 1){
-        cout<< "true"<<endl;
-    }else{
-        cout<<"false"<<endl;
+    if (next() == 1)
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+        cout << "false" << endl;
     }
     return 0;
 }
 
-bool next(){
+bool next()
+{
     return true;
 }
