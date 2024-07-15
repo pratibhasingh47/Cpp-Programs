7931a6f1fd67a19c34ab69b3000e74cbe0d4e69f
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-15 22:17:43 +0530

    oops

--- OOPS/new3.cpp
+++ OOPS/new3.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <cmath>
 
-class Triangle {
+class Triangle
+{
 public:
     double side1;
     double side2;
@@ -11,20 +12,23 @@ public:
     Triangle(double s1, double s2, double s3) : side1(s1), side2(s2), side3(s3) {}
 
     // Function to calculate and print the perimeter of the triangle
-    void printPerimeter() {
+    void printPerimeter()
+    {
         double perimeter = side1 + side2 + side3;
         std::cout << "Perimeter of the triangle: " << perimeter << " units" << std::endl;
     }
 
     // Function to calculate and print the area of the triangle using Heron's Formula
-    void printArea() {
+    void printArea()
+    {
         double s = (side1 + side2 + side3) / 2;
         double area = sqrt(s * (s - side1) * (s - side2) * (s - side3));
         std::cout << "Area of the triangle: " << area << " square units" << std::endl;
     }
 };
 
-int main() {
+int main()
+{
     // Create a Triangle object with sides 3, 4, and 5 units
     Triangle triangle(3, 4, 5);
 
