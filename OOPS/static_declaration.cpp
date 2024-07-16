3f7203a81a053828e6501900df227db6f551d3a1
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-16 22:15:32 +0530

    oops

--- OOPS/static_declaration.cpp
+++ OOPS/static_declaration.cpp
@@ -2,25 +2,29 @@
 using namespace std;
 class DSM
 {
-    private:
-    int a,b;
-    public:
+private:
+    int a, b;
+
+public:
     static int v1;
-    void read(int x,int y){
-        a=x;
-        b=y;
-        v1=v1+1;
+    void read(int x, int y)
+    {
+        a = x;
+        b = y;
+        v1 = v1 + 1;
     }
-    void show(){
-        cout << a << endl << b << endl;
+    void show()
+    {
+        cout << a << endl
+             << b << endl;
     }
 };
-int DSM :: v1 =0;
+int DSM ::v1 = 0;
 int main()
 {
-    DSM do1,do2;
-    do1.read(10,20);
-    do2.read(100,200);
+    DSM do1, do2;
+    do1.read(10, 20);
+    do2.read(100, 200);
     do1.show();
     do2.show();
     cout << do1.v1 << endl;
