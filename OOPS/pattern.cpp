c704964dffdbcf6193f46fd7da9dfc735c1735bb
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-16 22:15:04 +0530

    oops

--- OOPS/pattern.cpp
+++ OOPS/pattern.cpp
@@ -1,23 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
     int n = 4;
-    for(int i = 1 ; i <= n ; i++){
-        for(int j = 1; j <= i ; j++){
-            cout << j << " " ;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
         }
         cout << endl;
     }
 
     int b = 4;
-    for(int i = 1 ; i <= b ; i++){
-        for(int j = 1; j <= i ; j++){
-            if( j == i){
-                cout << j ;
+    for (int i = 1; i <= b; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (j == i)
+            {
+                cout << j;
             }
-            else{
-                cout << "_ " ;
+            else
+            {
+                cout << "_ ";
             }
         }
         cout << endl;
