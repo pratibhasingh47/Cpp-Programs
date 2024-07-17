6a65cc202edaf2fae7a070ed137a9475aabad3d0
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-17 21:16:39 +0530

    oops

--- OOPS/templateeg.cpp
+++ OOPS/templateeg.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 template <typename T, int size>
-class ArrayContainer {
+class ArrayContainer
+{
 private:
     T elements[size];
 
@@ -10,33 +11,44 @@ public:
     ArrayContainer() {}
 
     // Function to set elements
-    void setElement(int index, const T& value) {
-        if (index >= 0 && index < size) {
+    void setElement(int index, const T &value)
+    {
+        if (index >= 0 && index < size)
+        {
             elements[index] = value;
-        } else {
+        }
+        else
+        {
             std::cerr << "Index out of bounds!" << std::endl;
         }
     }
 
     // Function to get elements
-    T getElement(int index) const {
-        if (index >= 0 && index < size) {
+    T getElement(int index) const
+    {
+        if (index >= 0 && index < size)
+        {
             return elements[index];
-        } else {
+        }
+        else
+        {
             std::cerr << "Index out of bounds!" << std::endl;
             return T();
         }
     }
 };
 
-int main() {
+int main()
+{
     ArrayContainer<int, 5> intArray;
 
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 5; ++i)
+    {
         intArray.setElement(i, i * 10);
     }
 
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 5; ++i)
+    {
         std::cout << "Element at index " << i << ": " << intArray.getElement(i) << std::endl;
     }
 
