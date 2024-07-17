1be59bdc14d324636ad706651846eb14ab316111
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-17 21:17:21 +0530

    oops

--- OOPS/virtual.cpp
+++ OOPS/virtual.cpp
@@ -1,19 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class a1{
-    public:
+class a1
+{
+public:
     int x;
 };
-class b1{
-    public:
+class b1
+{
+public:
     int y;
 };
-class c1 : virtual public a1{
-    public:
+class c1 : virtual public a1
+{
+public:
     int z;
 };
-class d1:public c1,public b1{
-    public:
+class d1 : public c1, public b1
+{
+public:
     int x1;
 };
 main()
@@ -23,9 +27,9 @@ main()
     obj.y = 20;
     obj.z = 30;
     obj.x1 = 200;
-    cout << "x : "<< obj.x << endl;
-    cout << "y : "<< obj.y << endl;
-    cout << "z : "<< obj.z << endl;
-    cout << "x1 : "<< obj.x1 << endl;
+    cout << "x : " << obj.x << endl;
+    cout << "y : " << obj.y << endl;
+    cout << "z : " << obj.z << endl;
+    cout << "x1 : " << obj.x1 << endl;
     return 0;
 }
