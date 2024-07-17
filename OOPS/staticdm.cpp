f2e0f93b3250163fed29218ca2d42d83aac4e378
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-17 21:16:05 +0530

    oops

--- OOPS/staticdm.cpp
+++ OOPS/staticdm.cpp
@@ -1,28 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class item{
+class item
+{
     static int count;
     int num;
-    public:
-    void getcount(int i){
+
+public:
+    void getcount(int i)
+    {
         num = i;
         count++;
     }
-    void showcount(){
+    void showcount()
+    {
         cout << count << endl;
     };
 };
-int item :: count ;
+int item ::count;
 int main()
 {
-        item a,b,c;
-        a.showcount();
-        b.showcount(); 
-        c.showcount();
-        a.getcount(100);
-        b.getcount(200);
-        c.getcount(300);
-        a.showcount();
-        b.showcount();
-        c.showcount();
+    item a, b, c;
+    a.showcount();
+    b.showcount();
+    c.showcount();
+    a.getcount(100);
+    b.getcount(200);
+    c.getcount(300);
+    a.showcount();
+    b.showcount();
+    c.showcount();
 }
