9cc5209001d978f1da1cd861856057948f6a25a3
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-02 23:31:51 +0530

    cpp

---
+++ STL/list.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+void display(list<int> &int_list){
+    list<int> :: iterator it;
+    for(it = int_list.begin() ; it!= int_list.end() ; it++){
+        cout << *it << "  ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    list<int> list1;
+
+    list1.push_back(5);
+    list1.push_back(50);
+    list1.push_back(54);
+    list1.push_back(41);
+    list1.push_back(71);
+    list1.push_back(71);
+
+    display(list1);
+
+    list<int> :: iterator iter;
+    iter = list1.begin();
+    cout << *iter << "  ";
+    iter++;
+    cout << *iter << "  ";
+    iter++;
+    cout << *iter << "  ";
+    iter++;
+    cout << *iter << "  ";
+    iter++;
+    cout << *iter << "  ";
+    iter++;
+    cout << *iter << "  ";
+    iter++;
+    return 0;
+}
