0396f8dcece55af4adea22f31b42efe9cf2c2e24
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-02 22:44:48 +0530

    vector

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -1,11 +1,16 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-void display(vector<int> &v){
-    for(int i = 0; i< v.size() ; i++){
+template <class T>
+
+void display(vector<T> &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
         cout << v[i] << " ";
+        cout << v.at(i) << " ";
     }
     cout << endl;
 }
@@ -13,8 +18,10 @@ void display(vector<int> &v){
 int main()
 {
     vector<int> vec1;
+
     int element;
-    for(int i = 0 ; i< 4 ; i++){
+    for (int i = 0; i < 4; i++)
+    {
         cout << "Enter the element to add to this vector";
         cin >> element;
         vec1.push_back(element);
@@ -22,9 +29,26 @@ int main()
     vec1.pop_back();
     cout << "The vector is displayed as :" << endl;
     display(vec1);
+
     // vec1.insert(iterator , 786);
-    vector<int> :: iterator iter = vec1.begin();
-    vec1.insert(iter,786);
+
+    vector<int>::iterator iter = vec1.begin();
+    vec1.insert(iter, 5, 786);
     display(vec1);
+
+
+    vector<int> vec2(4);
+    vector<char> vec3(4);
+    vector<char> vec5(vec2);
+    vector<int> v(6,3);
+    vector<double> vec4(4);
+    // vec3.push_back('abcd');
+    // display(vec1);
+    // display(vec2);
+    // display(vec3);
+    // display(vec4);
+    display(v);
+
+    
     return 0;
 }
