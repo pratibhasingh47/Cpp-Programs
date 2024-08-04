5ecbe38a591411f21abcc6be7f2178e32996531f
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-04 17:41:01 +0530

    map

--- STL/map.cpp
+++ STL/map.cpp
@@ -6,6 +6,7 @@ int main()
     marksMap["Pratibha"] = 99;
     marksMap["Chitur pitur"] = 19;
     marksMap["Chitur pitur part 2"] = 17;
+    marksMap.insert({{"a",14},{"b",47}});
 
     map<string,int> :: iterator iter;
     for(iter = marksMap.begin(); iter!=marksMap.end(); iter++){
