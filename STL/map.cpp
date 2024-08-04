5c4f5f8ce61252d178a614e676402e115984cce3
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-08-04 17:30:48 +0530

    cpp

--- STL/map.cpp
+++ STL/map.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 int main()
 {
-    
+    map<string,int> marksMap;
+    marksMap['Pratibha'] = 99;
+    marksMap['Chitur pitur'] = 19;
+    marksMap['Chitur pitur part 2'] = 17;
+
     return 0;
 }
